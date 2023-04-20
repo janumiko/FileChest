b707145b96df907ecb9ece6e9b9dcf4557c09e1b
Kowalski1024 <tomasz_wojnar@hotmail.com>
2023-04-20 21:41:44 +0200

    chore: change from os to pathlib

--- filechest/settings.py
+++ filechest/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,5 +133,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = Path(BASE_DIR).joinpath("media")
 MEDIA_URL = "/media/"
