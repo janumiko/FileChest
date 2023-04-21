8a2189db6c681347a6cd46b4dc0755d967c0161e
BuBuSS <bubus258@gmail.com>
2023-04-22 01:39:19 +0200

    created navigations buttons

--- filechest_frontend/src/App.js
+++ filechest_frontend/src/App.js
@@ -3,34 +3,61 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-  state = {
-    folders: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      folders: [],
+      current_path: "",
+    };
+  }
 
   async componentDidMount()
   {
     try {
       const res = await fetch('http://127.0.0.1:8000/directory/', { method: 'GET' })
       const body = await res.json()
-      this.setState({folders: body[0]["folders"]}) 
+      this.setState({folders: body[0]["folders"]})
+      console.log("initial")
+      console.log(this.state.current_path)
     } catch (e) {
       console.log(e)
     }
   }
-  
+
+  async updateFolders(path) 
+  { 
+    this.state.current_path = path
+    try {
+      const res = await fetch('http://127.0.0.1:8000/directory/' + this.state.current_path, { method: 'GET' })
+      const body = await res.json()
+      this.setState({folders: body[0]["folders"]})
+      console.log(this.state.current_path)
+      console.log("update")
+      console.log(this.state.folders)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Welcome to FileChest!
           </p>
           {this.state.folders.map((folder) => (
-            <p key={folder}>
-              {folder}
-            </p>
+            <button key={folder + "button"} onClick={() => this.updateFolders(`${this.state.current_path}${folder}/`)}>
+              <p key={folder}>
+                {folder}
+              </p>
+            </button>
           ))}
+          <button key="return_button" onClick={() => this.updateFolders(this.state.current_path.split("/").slice(0, -2).join("/"))}>
+              <p key="return">
+                return
+              </p>
+            </button>
         </header>
       </div>
     );
