0044430a1b8821f2d90a1b21656fbea27d371aca
BuBuSS <bubus258@gmail.com>
2023-04-26 21:01:14 +0200

    Add NotFound page for non-existing endpoints

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './Home';
 import Folder from './Folder';
+import NotFound from './NotFound';
 
 const App = () => {
   return (
@@ -9,8 +10,9 @@ const App = () => {
       <Layout />
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/directory/*" element={<Folder />}></Route>
+          <Route path="/" element={<Home />}/>
+          <Route path="/directory/*" element={<Folder />}/>
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
       </Router>
     </div>
