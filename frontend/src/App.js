6b53920997628d6e95c7aa042fb81c9ee5205391
Kowalski1024 <tomasz_wojnar@hotmail.com>
2023-04-29 15:26:45 +0200

    chore: move router from App.js to router.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,8 @@
 import React from 'react';
-import {createBrowserRouter, RouterProvider, Link, Outlet, useLoaderData} from 'react-router-dom'
-import ErrorPage from "./components/Error";
-import NavBar from "./components/Navbar"
-import DirectoryContainer from "./components/DirectoryContainer";
+import {RouterProvider} from 'react-router-dom'
 
 
-const BACKEND_URL = 'http://localhost:8000';
-
-
-const router = createBrowserRouter([
-    {
-        element: <NavBar/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {
-                path: "/",
-                element: <div>Hello</div>,
-            },
-            {
-                path: '/directory/*',
-                element: <DirectoryContainer/>,
-                loader: async ({params}) => {
-                    let path = `${BACKEND_URL}/directory/${params['*']}`;
-                    console.log(path);
-                    const response = await fetch(path);
-
-                    if (response.status === 404) {
-                        throw new Response("Not Found", { status: 404 });
-                    }
-
-                    return await response.json();
-                }
-            },
-        ],
-    },
-]);
+import router from './router';
 
 
 export function App(props) {
